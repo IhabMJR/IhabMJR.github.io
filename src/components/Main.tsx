496911d1b800f96f973bc65a28052e97d9d3fc15
RandomizedRaptor <i.mseriousstuff@gmail.com>
2024-10-12 12:42:31 -0400

    fixed profile picture

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -9,7 +9,7 @@ function Main() {
     <div className="container">
       <div className="about-section">
         <div className="image-wrapper">
-          <img src="./assets/images/pfp.jpg" alt="Avatar" />
+          <img src="./src/assets/images/pfp.jpg" alt="Avatar" />
         </div>
         <div className="content">
           <div className="social_icons">
